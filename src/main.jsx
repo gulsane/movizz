f4344fab789a3ad001571bcd422bafcd86e4681e
gulsane <craftygulsane@gmail.com>
2023-10-03 17:02:53 +0530

    Added: Redux Store and configured it with app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider as ReduxProvider } from "react-redux";
 import App from "./App.jsx";
 import "./index.scss";
+import { store } from "./store/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<App />
+		<ReduxProvider store={store}>
+			<App />
+		</ReduxProvider>
 	</React.StrictMode>
 );
