ff3400d52bfc1b3e1c825b75e52f8e551e006b4b
gulsane <craftygulsane@gmail.com>
2023-10-06 00:21:42 +0530

    Added: description and overview of movie in DetailsBanner component

--- src/pages/details/detailsBanner/DetailsBanner.jsx
+++ src/pages/details/detailsBanner/DetailsBanner.jsx
@@ -11,7 +11,7 @@ import Genres from "../../../components/genres/Genres";
 import CircleRating from "../../../components/circleRating/CircleRating";
 import Img from "../../../components/lazyLoadImage/Img.jsx";
 import PosterFallback from "../../../assets/no-poster.png";
-// import { PlayIcon } from "../Playbtn";
+import { PlayIcon } from "../Playbtn";
 // import VideoPopup from "../../../components/videoPopup/VideoPopup";
 
 const DetailsBanner = ({ video, crew }) => {
@@ -57,6 +57,36 @@ const DetailsBanner = ({ video, crew }) => {
 										<Genres data={_genres} />
 										<div className="row">
 											<CircleRating rating={data.vote_average?.toFixed(1)} />
+											<div className="playbtn" onClick={() => {}}>
+												<PlayIcon />
+												<span className="text">Watch Trailer</span>
+											</div>
+										</div>
+										<div className="overview">
+											<div className="heading">Overview</div>
+											<div className="description">{data.overview}</div>
+										</div>
+										<div className="info">
+											{data.status && (
+												<div className="infoItem">
+													<span className="text bold">status: </span>
+													<span className="text">{data.status}</span>
+												</div>
+											)}
+											{data.release_date && (
+												<div className="infoItem">
+													<span className="text bold">Release Date: </span>
+													<span className="text">
+														{dayjs(data.release_date).format("MMM D, YYYY")}
+													</span>
+												</div>
+											)}
+											{data.runtime && (
+												<div className="infoItem">
+													<span className="text bold">Runtime: </span>
+													<span className="text">{toHoursAndMinutes(data.runtime)}</span>
+												</div>
+											)}
 										</div>
 									</div>
 								</div>
