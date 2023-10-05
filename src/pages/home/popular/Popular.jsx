9b1f15b83d7d5d865df335e0b4e0cd85c387702e
gulsane <craftygulsane@gmail.com>
2023-10-05 20:55:25 +0530

    Added: Popular carousel, which shows popular medias

---
+++ src/pages/home/popular/Popular.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import ComponentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import useFetch from "../../../hooks/useFetch";
+import Carousel from "../../../components/Carousel/Carousel";
+
+const Popular = () => {
+	const [endpoint, setEndpoint] = useState("movie");
+
+	const { data, loading } = useFetch(`/${endpoint}/popular`);
+
+	const onChange = (tab) => {
+		setEndpoint(tab === "Movies" ? "movie" : "tv");
+	};
+
+	return (
+		<div className="carouselSection">
+			<ComponentWrapper>
+				<span className="carouselTitle">What's Popular</span>
+				<SwitchTabs tabs={["Movies", "TV Shows"]} onTabChange={onChange} />
+			</ComponentWrapper>
+			<Carousel data={data?.results} loading={loading} endpoint={endpoint} />
+		</div>
+	);
+};
+
+export default Popular;
