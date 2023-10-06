185043b14bf2abef1d5d4a2bb7ac80950ed77667
gulsane <craftygulsane@gmail.com>
2023-10-06 14:04:11 +0530

    Implemented: data fetching for search query inside SearchResult component

--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,6 +1,31 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+
 import "./style.scss";
+
+import { fetchDataFromApi } from "../../utils/api";
+
 const SearchResult = () => {
-	return <div>SearchResult</div>;
+	const [data, setData] = useState(null);
+	const [pageNum, setPageNum] = useState(1);
+	const [loading, setLoading] = useState(false);
+	const { query } = useParams();
+
+	const fetchInitialData = () => {
+		setLoading(true);
+		fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`)
+			.then((res) => {
+				setData(res);
+				setPageNum((pre) => pre + 1);
+				setLoading(false);
+			})
+			.catch((err) => {});
+	};
+
+	useEffect(() => {
+		fetchInitialData();
+	}, [query]);
+	return <div className="SearchResultsPage">SearchResult</div>;
 };
 
 export default SearchResult;
