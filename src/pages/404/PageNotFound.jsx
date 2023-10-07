968449b57669fc26e775636b644fe3b43aaa97d7
gulsane <craftygulsane@gmail.com>
2023-10-07 13:35:19 +0530

    Modified: added logo for not found in PageNotFound component and styled it

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -1,12 +1,14 @@
 import "./style.scss";
 
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import noResult from "../../assets/no-results.png";
 
 const PageNotFound = () => {
 	return (
 		<div className="pageNotFound">
 			<ContentWrapper>
-				<span className="bigText">404</span>
+				<img className="notFoundImg" src={noResult} />
+				{/* <span className="bigText">404</span> */}
 				<span className="smallText">Page not found!</span>
 			</ContentWrapper>
 		</div>
