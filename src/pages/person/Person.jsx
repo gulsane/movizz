79c3b69b74b74322ff1ad56ad2bac07a55106131
gulsane <craftygulsane@gmail.com>
2023-10-18 12:19:20 +0530

    Added: poster image to person page

--- src/pages/person/Person.jsx
+++ src/pages/person/Person.jsx
@@ -1,7 +1,30 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 import "./style.scss";
 
+import useFetch from "../../hooks/useFetch";
+
+import Img from "../../components/lazyLoadImage/Img";
+
 const Person = () => {
-	return <div>Person</div>;
+	const { person_id } = useParams();
+	const { url } = useSelector((state) => state.home);
+	const { data, loading } = useFetch(`/person/${person_id}`);
+
+	console.log(data);
+
+	return (
+		<div className="personBanner">
+			{!loading && (
+				<div className="backdrop-img">
+					<Img src={`${url.profile}/${data?.profile_path}`} />
+				</div>
+			)}
+			<div className="opacity-layer"></div>
+		</div>
+	);
 };
 
 export default Person;
